1fd31b4af040097d94bc626cd2baa9c46e53e2db
jonazeiselt <jonazeiselt@gmail.com>
2017-01-23 03:02:13 +0100

    comments

--- SkickaEmail/SendEmail/SendEmail.ino
+++ SkickaEmail/SendEmail/SendEmail.ino
@@ -61,3 +61,17 @@ void loop()
   digitalWrite(2, LOW);    // turn the LED off by making the voltage LOW
   delay(1000);              // wait for a second
 }
+
+/* SMTP (Used for sending email)
+ * -----------------------------
+ * HELO smtp.gmail.com
+ * MAIL FROM: <museum@gmail.com>
+ * RCPT TO: <security@gmail.com>
+ * DATA
+ * Someone is too close to the painting!
+ * QUIT
+ */
+public void sendEmail()
+{
+
+}
