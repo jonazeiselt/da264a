8b350a26b993f8aca5524e961f005dca3432b2e1
jonazeiselt <jonazeiselt@gmail.com>
2017-01-23 02:45:31 +0100

    mac adress

--- SkickaEmail/SendEmail/SendEmail.ino
+++ SkickaEmail/SendEmail/SendEmail.ino
@@ -9,10 +9,12 @@
 
 #include "ESP8266WiFi.h"
 
-byte mac[] = { 0x00, 0x40, 0x8C, 0xEC, 0x60, 0xA5 };
-const char* ssid = "sauron";
+// byte mac[] = { 0x00, 0x40, 0x8C, 0xEC, 0x60, 0xA5 };
+char ssid[] = "ESP_D5D0B4";
 
-// IPAddress ip( 192, 168, 0, 1 );
+byte mac[6]; // MAC-address
+// IPAddress ip(192, 168, 0, 55);
+
 
 /* Modified code from http://www.instructables.com/id/IoT-ESP8266-Series-1-Connect-to-WIFI-Router/ */
 void setup()
@@ -21,15 +23,31 @@ void setup()
   // Connect to WiFi
   WiFi.begin(ssid);
 
+  // WiFi.config(ip);
   // while wifi not connected yet, print '.'
   // then after it connected, get out of the loop
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
-  
+
+
   Serial.println("\nWiFi connected");
   Serial.println(WiFi.localIP());  // IP address of ESP8266
+
+  WiFi.macAddress(mac);
+  Serial.print("MAC: ");
+  Serial.print(mac[5], HEX);
+  Serial.print(":");
+  Serial.print(mac[4], HEX);
+  Serial.print(":");
+  Serial.print(mac[3], HEX);
+  Serial.print(":");
+  Serial.print(mac[2], HEX);
+  Serial.print(":");
+  Serial.print(mac[1], HEX);
+  Serial.print(":");
+  Serial.println(mac[0], HEX);
 
   // initialize digital pin 13 as an output.
   pinMode(2, OUTPUT);
