08dda4e90d82eb0444f73ef7c4d95c6cac6e4334
axellundberg <axel0lundberg@gmail.com>
2017-01-27 11:21:42 +0100

    mail function

--- SkickaEmail/SendEmail/SendEmail.ino
+++ SkickaEmail/SendEmail/SendEmail.ino
@@ -12,18 +12,23 @@
 
 // byte mac[] = { 0x00, 0x40, 0x8C, 0xEC, 0x60, 0xA5 };
 char ssid[] = "iot";
-
+int port = 465;
 byte mac[6]; // MAC-address
 IPAddress ip(192, 168, 0, 55);
-IPAddress gateway(192,168,0,1);
-IPAddress subnet(255,255,255,0);
+IPAddress gateway(192, 168, 0, 1);
+IPAddress subnet(255, 255, 255, 0);
 
+char server[] = "smtp.gmail.com";
+
+WiFiClient client;
+
+/*https://www.lifewire.com/what-are-the-gmail-smtp-settings-1170854 */
 /* Modified code from http://www.instructables.com/id/IoT-ESP8266-Series-1-Connect-to-WIFI-Router/ */
 void setup()
 {
   Serial.begin(115200);
   // Connect to WiFi
-  WiFi.begin(ssid); 
+  WiFi.begin(ssid);
 
 
   WiFi.config(ip, gateway, subnet);
@@ -54,6 +59,8 @@ void setup()
 
   // initialize digital pin 13 as an output.
   pinMode(2, OUTPUT);
+
+  sendEmail();
 }
 
 // the loop function runs over and over again forever
@@ -73,8 +80,36 @@ void loop()
  * DATA
  * Someone is too close to the painting!
  * QUIT
+ * https://forum.arduino.cc/index.php?topic=126829.0 modified
  */
 void sendEmail()
 {
-
+  client.connect(server,port);
+  Serial.println("connected");
+  client.println("EHLO www.gmail.com");
+  client.println("AUTH LOGIN");
+  client.println("Ym9iZ3VhcmQ0NUBnbWFpbC5jb20="); // mail
+  client.println("bWFyeWphbmU0NQ==");     //pass
+  client.println("MAIL FROM:<bobguard45@gmail.com>");
+  client.println("RCPT TO:<jonazeiselt@gmail.com>");
+  client.println("DATA");
+  client.println("from:bobguard45@gmail.com");
+  client.println("to:axel0lundberg@gmail.com");
+  client.println("SUBJECT: Warning!");
+  client.println();
+  client.println("Someone is too close to painting x.");
+  client.println(".");
+  client.println(".");
+  client.println("QUIT");
+  if (client.available()) {
+    char a = client.read();
+    Serial.print(a);
+  }
 }
+
+void smtpResponse()
+{
+  String res = client.readStringUntil('\n');
+  Serial.println(res);
+}
+
