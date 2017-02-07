ec043a6f0b8d8ba750e45ef74b9da54e15662bf6
danial2016 <danial_mahmoud@hotmail.com>
2017-02-07 12:34:29 +0100

    Ultrasensor har integrerats med sensorsystemet

---
+++ SensorClasses/UltrasensorTest/UltrasensorTest.ino
@@ -0,0 +1,76 @@
+/* HC-SR04 Sensor
+  
+   This sketch reads a HC-SR04 ultrasonic rangefinder and returns the
+   distance to the closest object in range. To do this, it sends a pulse
+   to the sensor to initiate a reading, then listens for a pulse 
+   to return.  The length of the returning pulse is proportional to 
+   the distance of the object from the sensor.
+     
+   The circuit:
+  * VCC connection of the sensor attached to +5V
+  * GND connection of the sensor attached to ground
+  * TRIG connection of the sensor attached to digital pin 2
+  * ECHO connection of the sensor attached to digital pin 4
+   Original code for Ping))) example was created by David A. Mellis
+   Adapted for HC-SR04 by Tautvidas Sipavicius
+   This example code is in the public domain.
+ */
+
+
+const int trigPin = 12;
+const int echoPin = 14;
+const int redLED = 4;
+
+void setup() {
+  // initialize serial communication:
+  Serial.begin(115200);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(redLED, OUTPUT);
+}
+
+void loop()
+{
+  // establish variables for duration of the ping, 
+  // and the distance result in inches and centimeters:
+  long duration, inches, cm;
+
+  // The sensor is triggered by a HIGH pulse of 10 or more microseconds.
+  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
+  
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  // Read the signal from the sensor: a HIGH pulse whose
+  // duration is the time (in microseconds) from the sending
+  // of the ping to the reception of its echo off of an object.
+  
+  duration = pulseIn(echoPin, HIGH);
+
+  // convert the time into a distance
+  cm = microsecondsToCentimeters(duration);
+  
+  Serial.print("Distance to object: ");
+  Serial.print(cm);
+  Serial.print("cm");
+  Serial.println();
+
+  if(cm <= 20){
+    digitalWrite(redLED, HIGH);
+  }else{
+    digitalWrite(redLED, LOW);
+  }
+  
+  delay(300);
+}
+
+long microsecondsToCentimeters(long microseconds)
+{
+  // The speed of sound is 340 m/s or 29 microseconds per centimeter.
+  // The ping travels out and back, so to find the distance of the
+  // object we take half of the distance travelled.
+  return microseconds / 29 / 2;
+}
